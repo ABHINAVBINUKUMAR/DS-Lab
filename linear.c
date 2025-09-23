9f51e87324b9a4c20b7f0cd570a51ae027629eb2
ABHINAVBINUKUMAR <abhinavabhi03739@gmail.com>
2025-09-23 14:38:01 +0530

    Add files via upload

---
+++ linear.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int i,n,key,found=0;
+printf("enter the size of the array :");
+scanf("%d",&n);
+int arr[n];
+printf("enter %d elements:\n",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("Enter element to search:");
+scanf("%d",&key);
+for(i=0;i>n;i++){
+if(arr[i]==key)
+{
+printf("element %d found at position %d\n",key,i+1);
+found = 1;
+}
+}
+if(found==0)
+{
+printf("Element %d not found in the array\n",key);
+}
+return 0;
+}
+
